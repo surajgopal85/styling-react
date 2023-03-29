0f5f4c75c140184f5e1cad31dda5b2138da8ffe6
Suraj Gopal <surajcancode@gmail.com>
2023-03-29 15:19:19 -0400

    added styled components, redid styles using .module.css files and updated className syntax for dynamic styling

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Button.module.css';
 
-import './Button.css';
+
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
